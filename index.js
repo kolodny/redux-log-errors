15e58739be6243c387827a97acc5470a56c335a4
Moshe Kolodny <kolodny.github@gmail.com>
2015-12-30 11:09:36 -0500

    intial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var xtends = Object.assign || function (target, source) {
+  for (var key in source) {
+    if (Object.prototype.hasOwnProperty.call(source, key)) {
+      target[key] = source[key];
+    }
+  }
+  return target;
+};
+
+exports['default'] = function (createStore) {
+  return function (reducer, initialState) {
+    var store = createStore(reducer, initialState);
+    var dispatch = store.dispatch;
+
+    return xtends({}, store, {
+      dispatch: function dispatch() {
+        try {
+          return dispatch.apply(undefined, arguments);
+        } catch (error) {
+          console.error('an error occurred while dispatching', { error: error });
+        }
+      }
+    });
+  };
+};
+
+module.exports = exports['default'];
