9bf19a63ed71956293910ce913763bf0f55ec0fd
Moshe Kolodny <kolodny.github@gmail.com>
2015-12-30 11:26:41 -0500

    npm link next time ;)

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ var xtends = Object.assign || function (target, source) {
 exports['default'] = function (createStore) {
   return function (reducer, initialState) {
     var store = createStore(reducer, initialState);
-    var dispatch = store.dispatch;
+    var cachedDispatch = store.dispatch;
 
     return xtends({}, store, {
       dispatch: function dispatch() {
         try {
-          return dispatch.apply(undefined, arguments);
+          return cachedDispatch.apply(undefined, arguments);
         } catch (error) {
           console.error('an error occurred while dispatching', { error: error });
         }
